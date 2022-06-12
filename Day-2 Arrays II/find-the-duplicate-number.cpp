b676c800cac82c2d32889df4df05e48d78218a47
saumya81 <40277007+saumya81@users.noreply.github.com>
2022-06-12 17:48:39 +0530

    Create find-the-duplicate-number.cpp

---
+++ Day-2 Arrays II/find-the-duplicate-number.cpp
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/find-the-duplicate-number/
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+       
+        for(int i=0;i<nums.size();i++)
+        {
+           if(nums[abs(nums[i])]>=0)
+                nums[abs(nums[i])]=- nums[abs(nums[i])];
+            else return abs(nums[i]);
+        }
+        return -1;
+       
+    }
+};
+
